c902f6e7823e2dcf59c5afc35469c286722e58de
Khanh Nguyen <xuankhanh3393@gmail.com>
2022-05-31 23:03:30 +0700

    convert branch name to Không dấu

--- frontend/module/git-actions/git-actions.service.ts
+++ frontend/module/git-actions/git-actions.service.ts
@@ -47,6 +47,20 @@ export class GitActionsService {
       .replace(/^[-]+/g, '') // Dashes at the start are removed
       .replace(/[-]+$/g, '') // Dashes at the end are removed
       .replace(/-+/g, '-') // Multiple dashes in a row are deduped
+      .replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a')
+      .replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e')
+      .replace(/(ì|í|ị|ỉ|ĩ)/g, 'i')
+      .replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o')
+      .replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u')
+      .replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y')
+      .replace(/(đ)/g, 'd')
+      .replace(/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/g, 'A')
+      .replace(/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/g, 'E')
+      .replace(/(Ì|Í|Ị|Ỉ|Ĩ)/g, 'I')
+      .replace(/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/g, 'O')
+      .replace(/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/g, 'U')
+      .replace(/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/g, 'Y')
+      .replace(/(Đ)/g, 'D')
       .trim();
   }
 
